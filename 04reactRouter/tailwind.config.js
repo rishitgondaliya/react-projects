81c86451a03cfb2387120a0a116f50672b309b55
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-07-25 22:01:57 +0530

    added new path

--- 04reactRouter/tailwind.config.js
+++ 04reactRouter/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JS, JSX, TS, and TSX files in src
+    './components/**/*.{js,jsx,ts,tsx}', // Include all JS, JSX, TS, and TSX files in components
+    './index.html', // Include the HTML file at the root level
   ],
   theme: {
     extend: {},
