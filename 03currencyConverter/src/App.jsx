a492855f4131377aec620c823bfdccef0c051fcb
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-10 21:08:35 +0530

    style changed

--- 03currencyConverter/src/App.jsx
+++ 03currencyConverter/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
                 onAmountChange={(amount) => setAmount(amount)}
               />
             </div>
-            <div className="w-full h-0.5 mt-8">
+            <div className="w-full h-0.5">
               <button
                 type="button"
                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
