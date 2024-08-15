76c7104ff07c56ba52b947375c3d76cbeb3e3b7f
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-15 10:48:51 +0530

    added different method

--- 05miniContext/src/context/UserContext.js
+++ 05miniContext/src/context/UserContext.js
@@ -3,3 +3,27 @@ import React from 'react'
 const UserContext = React.createContext()
 
 export default UserContext
+
+// import { createContext, useContext, useState } from "react";
+
+// // Create the UserContext with default values
+// export const UserContext = createContext({
+//     user: null,
+//     setUser: () => {},
+// });
+
+// // Create the UserProvider component
+// export const UserProvider = ({ children }) => {
+//     const [user, setUser] = useState(null);
+
+//     return (
+//         <UserContext.Provider value={{ user, setUser }}>
+//             {children}
+//         </UserContext.Provider>
+//     );
+// };
+
+// // Custom hook to use the UserContext
+// export default function useUser() {
+//     return useContext(UserContext);
+// }
