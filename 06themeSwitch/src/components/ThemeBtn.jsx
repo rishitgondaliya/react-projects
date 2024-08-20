a6b070f278f1ba181e2ad5df0d35b2fe09a9c062
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-20 13:35:31 +0530

    modified

--- 06themeSwitch/src/components/ThemeBtn.jsx
+++ 06themeSwitch/src/components/ThemeBtn.jsx
@@ -18,7 +18,7 @@ export default function ThemeBtn() {
                 value=""
                 className="sr-only peer"
                 onChange={onChangeBtn}
-                checked={themeMode=== "dark"}
+                checked={themeMode === "dark"}
             />
             <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4
              peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer
