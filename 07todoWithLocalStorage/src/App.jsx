63ea11eb01a26a8dcc79df48fb52c7a17d6113ee
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-28 17:36:02 +0530

    modified style

--- 07todoWithLocalStorage/src/App.jsx
+++ 07todoWithLocalStorage/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <TodoProvider value={{ todos, addToDo, updateToDo, deleteToDo, toggleComplete }}>
-      <div className="bg-[#1e3559] min-h-screen py-8">
+      <div className="bg-[#1e3558] min-h-screen py-8">
         <div className="bg-[#172842] w-full max-w-2xl mx-auto shadow-lg rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
