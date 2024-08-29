c11e28a6e3b167738ccdf74fbd484fc0248f89c0
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-08-29 19:02:40 +0530

    designed add todo component

--- 08todoWithRedux/src/components/AddTodo.jsx
+++ 08todoWithRedux/src/components/AddTodo.jsx
@@ -1,31 +1,31 @@
-// import { useState } from "react"
-// import { useDispatch } from 'react-redux'
-// import { addTodo } from "../features/todo/todoSlice"
+import { useState } from "react"
+import { useDispatch } from 'react-redux'
+import { addTodo } from "../features/todo/todoSlice"
 
-// export default function AddTodo() {
-//     const [todo, setTodo] = useState("")
-//     const dispatch = useDispatch()
+export default function AddTodo() {
+    const [todo, setTodo] = useState("")
+    const dispatch = useDispatch()
 
-//     const addTodoHandler = (e) => {
-//         e.preventDefault()
-//         dispatch(addTodo(todo))
-//         setTodo("")
-//     }
+    const addTodoHandler = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(todo))
+        setTodo("")
+    }
 
-//     return (
-//         <>
-//             <form onSubmit={addTodoHandler} className="flex">
-//                 <input
-//                     type="text"
-//                     placeholder="Write Todo..."
-//                     className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
-//                     value={todo}
-//                     onChange={(e) => setTodo(e.target.value)}
-//                 />
-//                 <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
-//                     Add
-//                 </button>
-//             </form>
-//         </>
-//     )
-// }
+    return (
+        <>
+            <form onSubmit={addTodoHandler} className="flex">
+                <input
+                    type="text"
+                    placeholder="Write Todo..."
+                    className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                    value={todo}
+                    onChange={(e) => setTodo(e.target.value)}
+                />
+                <button type="submit" className="w-16 rounded-r-lg px-3 py-1 bg-green-600 text-white text-lg shrink-0">
+                    Add
+                </button>
+            </form>
+        </>
+    )
+}
