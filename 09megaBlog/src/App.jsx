7d204dafa5c6745c5469f25db3c7674b60e5a5bd
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-06 10:25:14 +0530

    added loading functionality

--- 09megaBlog/src/App.jsx
+++ 09megaBlog/src/App.jsx
@@ -1,12 +1,39 @@
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-    <h1>A blog app with appwrite</h1>
-    </>
-  ) 
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  // conditional rendering
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : <div className="else"></div>
 }
 
 export default App
