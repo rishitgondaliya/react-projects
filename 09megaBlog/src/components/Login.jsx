9fead3cce0738d94a1cc8335aa927176e6097ea8
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-09 01:13:14 +0530

    syntax correction

--- 09megaBlog/src/components/Login.jsx
+++ 09megaBlog/src/components/Login.jsx
@@ -36,7 +36,7 @@ function Login() {
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
                 <p className="text-center mt-2 text-base text-black/60">
-                    Don&apos;t have an account ?&nsbp;
+                    Don&apos;t have an account ?&nbsp;
                     <Link to="/signup"
                         className="font-medium text-primary text-blue-500 hover:text-blue-700 transition-all duration-200 ease-in-out">
                         Sign up
