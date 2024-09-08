6e63406b70bb8ceef3de608994956b254961e8aa
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-09 01:12:32 +0530

    export all components

--- 09megaBlog/src/components/index.js
+++ 09megaBlog/src/components/index.js
@@ -3,8 +3,15 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from './Login'
 import Button from './Button'
 import Input from './Input'
+import Select from './Select'
+import RTE from './RTE'
+import Signup from './Signup'
+import PostCard from './Postcard'
+import PostForm from './post-form/PostForm'
+import AuthLayout from './AuthLayout'
 
 export {
     Header,
@@ -12,6 +19,13 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    Login,
     Button,
-    Input
+    Input,
+    Select,
+    RTE,
+    Signup,
+    PostCard,
+    PostForm,
+    AuthLayout
 }
