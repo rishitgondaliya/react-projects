6a4a5a8e6c0364d897c87b062c93b3fcc440d989
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-09 01:11:58 +0530

    modified logo

--- 09megaBlog/src/components/Logo.jsx
+++ 09megaBlog/src/components/Logo.jsx
@@ -1,6 +1,11 @@
-export default function Logo(){
-    return(
-        <>
-        </>
-    )
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div>Logo</div>
+  )
 }
+
+export default Logo
