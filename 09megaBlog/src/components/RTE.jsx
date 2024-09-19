fc5dbf1756c09d9705c4b65b328e20bd6d72deb2
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-19 20:14:04 +0530

    hide sensitive info

--- 09megaBlog/src/components/RTE.jsx
+++ 09megaBlog/src/components/RTE.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
+import conf from '../conf/conf'
 
 export default function RTE({ name, control, label, defaultValue = "" }) {
     return (
@@ -17,7 +18,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                        apiKey='66tw0itmoetr3cptmn020rnfocy2g6d4yhnbeqka28w401wr'
+                        apiKey={conf.tinymceApiKey}
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
