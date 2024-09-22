9dfd38931b7724495b86ed592263d47e460c441e
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-22 19:07:28 +0530

    managed loading state

--- 09megaBlog/src/pages/AllPosts.jsx
+++ 09megaBlog/src/pages/AllPosts.jsx
@@ -1,31 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect, useState } from 'react'
-import { Container, PostCard } from '../components/index'
-import appwriteService from '../appwrite/config'
+import React, { useEffect, useState } from 'react';
+import { Container, PostCard } from '../components/index';
+import appwriteService from '../appwrite/config';
 
 function AllPosts() {
-  const [posts, setPosts] = useState([])
-  useEffect(() => { }, [])
-  appwriteService.getPosts([])
-    .then((posts) => {
-      if (posts) {
-        setPosts(posts.documents)
-      }
-    })
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    appwriteService.getPosts([]) 
+      .then((response) => {
+        if (response) {
+          setPosts(response.documents);
+        }
+        setLoading(false);
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  }, []); 
+
+  if (loading) {
+    return <div className="w-full py-8 mt-4 text-center">
+    <Container>
+        <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+                <h1 className="text-2xl font-bold hover:text-gray-500">
+                    Loading blogs for you...
+                </h1>
+            </div>
+        </div>
+    </Container>
+</div>
+  }
+
+  if (!posts.length) {
+    return <div className="text-center text-3xl my-8">No blogs available</div>;
+  }
 
   return (
     <div className="w-full py-8">
       <Container>
         <div className="flex flex-wrap">
-          {posts.map((post) => {
-            <div key={post.$id} className='p-2 w-1/4'>
-              <PostCard post={post} />
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
             </div>
-          })}
+          ))}
         </div>
       </Container>
     </div>
-  )
+  );
 }
 
-export default AllPosts
+export default AllPosts;
