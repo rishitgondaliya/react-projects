c6f746892b77a17135b5a2c020af258480b8437d
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-24 19:23:49 +0530

    bg changed

--- 09megaBlog/src/App.jsx
+++ 09megaBlog/src/App.jsx
@@ -1,3 +1,45 @@
+// import { useState, useEffect } from 'react'
+// import { useDispatch } from 'react-redux'
+// import { Outlet } from 'react-router-dom'
+// import { login, logout } from './store/authSlice'
+// import { Header, Footer } from './components/index'
+// import authService from './appwrite/auth'
+// import './App.css'
+
+// function App() {
+//   const [loading, setLoading] = useState(true)
+//   const dispatch = useDispatch()
+
+//   useEffect(() => {
+//     authService.getCurrentUser()
+//       .then((userData) => {
+//         if (userData) {
+//           dispatch(login({ userData }))
+//         } else {
+//           dispatch(logout())
+//         }
+//       })
+//       .finally(() => setLoading(false))
+//   }, [])
+
+//   // conditional rendering
+//   return !loading ? (
+//     <div className="min-h-screen flex flex-wrap content-between"
+//     style={{backgroundImage: 'linear-gradient(to right, #9aabff, #48e4dc)'}}>
+//       <div className="w-full block">
+//         <Header />
+//         <main className='p-8'>
+//           <Outlet />
+//         </main>
+//         <Footer />
+//       </div>
+//     </div>
+//   ) : null
+// }
+
+// export default App
+
+
 import { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Outlet } from 'react-router-dom'
@@ -14,20 +56,22 @@ function App() {
     authService.getCurrentUser()
       .then((userData) => {
         if (userData) {
+          // Dispatch login if userData exists
           dispatch(login({ userData }))
         } else {
           dispatch(logout())
         }
       })
       .finally(() => setLoading(false))
-  }, [])
+  }, [dispatch])
 
   // conditional rendering
   return !loading ? (
-    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className="min-h-screen flex flex-wrap content-between"
+    style={{backgroundImage: 'linear-gradient(to right, #9aabff, #48e4dc)'}}>
       <div className="w-full block">
         <Header />
-        <main>
+        <main className='p-8'>
           <Outlet />
         </main>
         <Footer />
