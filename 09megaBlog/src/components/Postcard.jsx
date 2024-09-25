0a9f53db0d10fe7a12f202405d92ad0419000ae6
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-09-25 19:09:51 +0530

    added fallback image

--- 09megaBlog/src/components/Postcard.jsx
+++ 09megaBlog/src/components/Postcard.jsx
@@ -8,9 +8,9 @@ function Postcard({ $id, title, featuredImage }) {
         : "https://static.vecteezy.com/system/resources/previews/036/226/872/non_2x/ai-generated-nature-landscapes-background-free-photo.jpg";
     return (
         <Link to={`/post/${$id}`}>
-            <div className="bg-gray-200 w-full rounded-xl p-4">
+            <div className="bg-[#f1f0d8] w-full rounded-xl p-4">
                 <div className="w-full justify-center mb-4">
-                    <img src={imageUrl} alt={title} className="rounnded-xl" />
+                    <img src={imageUrl} alt={title} className="rounnded-xl"/>
                 </div>
                 <h2 className="text-xl font-bold">{title}</h2>
             </div>
